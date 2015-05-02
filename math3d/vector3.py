17e0bd83a875d618b5b94afcccbd52488324b8a4
Jonas Rabbe <jonas.rabbe@gmail.com>
2015-05-01 17:51:07 -0700

    Adds some doc strings on the vector functions

--- math3d/vector3.py
+++ math3d/vector3.py
@@ -5,6 +5,9 @@ from __future__ import division
 
 import math
 
+def create(x, y, z):
+    return Vector(x, y, z)
+
 def normalize(vector):
     result = vector.clone()
     return result.normalize()
@@ -22,16 +25,22 @@ def distance_squared(a, b):
     return (a - b).lengthSquared()
 
 def distance(a, b):
+    """
+    Calculates the disctance between the two provided vectors
+    """
     return len(a - b)
 
 def zero():
+    """
+    Creates a zero vector, i.e. a vector with length zero
+    """
     return Vector()
 
 def up():
+    """
+    Creates a new 3D unit vector pointing up along the y axis.
+    """
     return Vector(y=1.0)
-
-def swap(a, b):
-    return (b, a)
 
 class Vector(object):
 
@@ -94,6 +103,10 @@ class Vector(object):
         return Vector(self.x, self.y, self.z)
 
     def cross(self, other):
+        """
+        Calculates the cross product of this vector against the provided,
+        returning a new vector with the result.
+        """
         result = Vector()
         result.x = self.y * other.z - self.z * other.y
         result.y = self.z * other.x - self.x * other.z
@@ -101,9 +114,18 @@ class Vector(object):
         return result
 
     def dot(self, other):
+        """
+        Calculates the dot product of this vector against the other returning
+        the calculated value.
+        """
         return self.x * other.x + self.y * other.y + self.z * other.z
 
     def transform(self, transformation):
+        """
+        Performs the transformation of the values for this vector with the
+        provided transformation matrix, returning a new vector with the
+        transformed values.
+        """
         x = (self.x * transformation.m[0]) + (self.y * transformation.m[4]) + (self.z * transformation.m[8]) + transformation.m[12]
         y = (self.x * transformation.m[1]) + (self.y * transformation.m[5]) + (self.z * transformation.m[9]) + transformation.m[13]
         z = (self.x * transformation.m[2]) + (self.y * transformation.m[6]) + (self.z * transformation.m[10]) + transformation.m[14]
